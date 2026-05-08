c48c6e2c9c2cdb4bf3ef42f49ae2cc5a484c05dc
Vignesh Mac <vigneshvasudevan1@outlook.com>
2026-05-08 15:25:17 +0200

    Configuring cucumber test runner

--- src/test/java/com/booking/runners/TestRunner.java
+++ src/test/java/com/booking/runners/TestRunner.java
@@ -1,14 +1,16 @@
 package com.booking.runners;
-
-import org.junit.platform.suite.api.*;
-import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("com.booking")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.booking")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html, json:target/cucumber.json")
 public class TestRunner {
+
 }

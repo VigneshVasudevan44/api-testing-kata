9556cc483984e88617cabf2f02b0cb2aeee44ce1
Vignesh Mac <vigneshvasudevan1@outlook.com>
2026-05-08 02:19:22 +0200

    Adding Rest-Assured configuration and Cucumber hooks

--- src/test/java/com/booking/config/TestConfig.java
+++ src/test/java/com/booking/config/TestConfig.java
@@ -1,4 +1,18 @@
 package com.booking.config;
+import io.restassured.RestAssured;
 
 public class TestConfig {
+
+    /**
+     * Centralized Rest-Assured Configuration
+     * Used Across All test scenarios
+     */
+    public static void setup() {
+
+        // Base URI for booking application APIs
+        RestAssured.baseURI = "https://automationintesting.online/api";
+
+        // Logs request and response details automatically on failures
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
 }
--- src/test/java/com/booking/hooks/Hooks.java
+++ src/test/java/com/booking/hooks/Hooks.java
@@ -1,4 +1,16 @@
 package com.booking.hooks;
+import com.booking.config.TestConfig;
+import io.cucumber.java.Before;
 
 public class Hooks {
+
+    /**
+     * Cucumber hooks executed before test scenarios.
+     */
+    @Before
+    public void setup() {
+
+        // Initialize framework configuration before each scenario
+        TestConfig.setup();
+    }
 }
